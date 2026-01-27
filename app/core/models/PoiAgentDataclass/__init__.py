29f1b647914162decd27248afb4c8c318d0ff645
Kskullknight <k010302jsu@naver.com>
2026-01-27 13:19:16 +0900

    feat: add PoiDatamodel

---
+++ app/core/models/PoiAgentDataclass/__init__.py
@@ -0,0 +1,17 @@
+from app.core.models.PoiAgentDataclass.poi import (
+    PoiCategory,
+    PoiSource,
+    PoiData,
+    PoiSearchResult,
+    PoiInfo,
+    PoiAgentState,
+)
+
+__all__ = [
+    "PoiCategory",
+    "PoiSource",
+    "PoiData",
+    "PoiSearchResult",
+    "PoiInfo",
+    "PoiAgentState",
+]
